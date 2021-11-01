d5d2f751d177c47c75546767c42338ad65abc1f5
M-Bohdan <melexinbo@gmail.com>
2021-11-01 20:56:23 +0200

    add file

--- code/Mageplaza/HelloWorld/Controller/Index/Index.php
+++ code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -7,26 +7,6 @@ use Magento\Framework\Controller\ResultFactory;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-  // protected $pageFactory;
-
-  // public function __construct(
-  //   \Magento\Framework\App\Action\Context $context,
-  //   \Magento\Framework\View\Result\PageFactory $pageFactory
-  // ) {
-  //   $this->pageFactory = $pageFactory;
-  //   parent::__construct($context);
-  // }
-
-  // public function execute()
-  // {
-  //   $page = $this->pageFactory->create();
-  //   return $page;
-  // }
-
-  // public function execute()
-  // {
-  //   die("Hello World");
-  // }
   public function execute()
   {
     $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
