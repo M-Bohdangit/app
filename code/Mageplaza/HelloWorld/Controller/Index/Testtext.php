6a141839746843fd6246ec2d1dbaabe765039e3b
M-Bohdan <melexinbo@gmail.com>
2021-11-02 13:30:33 +0200

    add file

---
+++ code/Mageplaza/HelloWorld/Controller/Index/Testtext.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mageplaza\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+class Testtext extends Action
+{
+  public function execute()
+  {
+    die("Controller1_1");
+  }
+}
