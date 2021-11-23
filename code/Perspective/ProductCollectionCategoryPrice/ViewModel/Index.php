3ee9dbc6143e1025f4e3b61ce3551acadb406848
M-Bohdan <melexinbo@gmail.com>
2021-11-24 00:31:47 +0200

    add fille

--- code/Perspective/ProductCollectionCategoryPrice/ViewModel/Index.php
+++ code/Perspective/ProductCollectionCategoryPrice/ViewModel/Index.php
@@ -7,29 +7,28 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 class Index implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
 
-  /**
-   * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
-   */
-  protected $_productCollectionFactory;
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     */
+    protected $productCollectionFactory;
 
-  public function __construct(
-    \Magento\Backend\Block\Template\Context $context,
-    \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
-    array $data = []
-  ) {
-    $this->_productCollectionFactory = $productCollectionFactory;
-  }
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
 
-  /**
-   * 
-   * @param array $ids 
-   * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
-   */
-  public function getProductCollectionByCategories(array $ids)
-  {
-    $collection = $this->_productCollectionFactory->create();
-    $collection->addAttributeToSelect('*');
-    $collection->addCategoriesFilter(['in' => $ids]);
-    return $collection;
-  }
+    ) {
+        $this->productCollectionFactory = $productCollectionFactory;
+    }
+
+    /**
+     * 
+     * @param array $ids 
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
+     */
+    public function getProductCollectionByCategories(array $ids)
+    {
+        $collection = $this->productCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+        $collection->addCategoriesFilter(['in' => $ids]);
+        return $collection;
+    }
 }
