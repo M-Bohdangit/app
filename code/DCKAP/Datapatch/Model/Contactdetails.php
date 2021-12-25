2f27a5c4cb183e42467459054ea64d3ada8789bf
M-Bohdan <melexinbo@gmail.com>
2021-12-25 19:47:37 +0200

    add fille

---
+++ code/DCKAP/Datapatch/Model/Contactdetails.php
@@ -0,0 +1,10 @@
+<?php
+namespace DCKAP\Datapatch\Model;
+use Magento\Framework\Model\AbstractModel;
+class Contactdetails extends AbstractModel{
+protected function _construct()
+{
+$this->_init
+("DCKAP\Datapatch\Model\ResourceModel\Contactdetails");
+}
+}
